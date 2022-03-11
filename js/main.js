dbabbc851a482be0efc32ddce2c4ecd8a95ef9aa
Nigel Hernandez <99218651+Nigel0330@users.noreply.github.com>
2022-03-11 22:36:10 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const wordsTextarea = document.querySelector(".word-counter-container .words");
+const countBtn = document.querySelector(".word-counter-container .count-btn");
+const wordCount = document.querySelector(".word-counter-container .word-count span");
+
+const countWords =  () => {
+	let words = wordsTextarea.value;
+	let wordsTrimmed = words.replace(/\s+/g, " ").trim
+	();
+	let splitWords = wordsTrimmed.split(" ");
+
+	console.log(splitWords);
+
+	let numberofWords = splitWords.length;
+	if(splitWords[0] == "") {
+		numberofWords = 0;
+	}
+
+	wordCount.innerHTML = numberofWords;
+};
+
+countBtn.addEventListener("click", countWords);
